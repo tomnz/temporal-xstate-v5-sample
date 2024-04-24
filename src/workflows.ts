4f0cf990a691f6271e57be946a0e6fe977ef65e8
Tom <tom@tom.net.nz>
2024-04-24 08:56:28 -0700

    Formatting

--- src/workflows.ts
+++ src/workflows.ts
@@ -1,15 +1,21 @@
 import { log, setHandler } from "@temporalio/workflow";
 import { createActor, getInitialSnapshot } from "xstate";
 
-import { type CaseMachineState, caseMachine, stateQuery, closeSignal, escalateSignal} from "./machines";
+import {
+  type CaseMachineState,
+  caseMachine,
+  stateQuery,
+  closeSignal,
+  escalateSignal,
+} from "./machines";
 
 export async function caseEngine() {
   const actor = createActor(caseMachine);
 
   let state: CaseMachineState = getInitialSnapshot(caseMachine);
-  actor.subscribe(nextState => {
-      state = nextState;
-      log.info(`Machine state: ${JSON.stringify(nextState, null, 2)}`);
+  actor.subscribe((nextState) => {
+    state = nextState;
+    log.info(`Machine state: ${JSON.stringify(nextState, null, 2)}`);
   });
   setHandler(stateQuery, () => state);
 
@@ -20,14 +26,14 @@ export async function caseEngine() {
   const send = actor.send.bind(actor);
 
   setHandler(closeSignal, () => send({ type: "close" }));
-  setHandler(escalateSignal, props => send({ type: "escalate", ...props }));
+  setHandler(escalateSignal, (props) => send({ type: "escalate", ...props }));
 
-  await new Promise<void>(resolve => {
-      actor.subscribe({
+  await new Promise<void>((resolve) => {
+    actor.subscribe({
       complete() {
-          log.info("Machine completed");
-          resolve();
+        log.info("Machine completed");
+        resolve();
       },
-      });
+    });
   });
 }
